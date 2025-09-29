e509836b4075e0a147f8a3f5ead81cf2d1577d6e
Akshat Soni <19uec087@lnmiit.ac.in>
2025-09-29 16:24:16 +0530

    Using parser to catch input

--- Task-Tracker/main.py
+++ Task-Tracker/main.py
@@ -1,4 +1,7 @@
 import json
+import argparse
+import os
+from datetime import datetime
 
 file_path = "tasks_list.json"
    
@@ -9,33 +12,55 @@ def check_file(file_path):
             pass
     except FileExistsError:
         pass
+
+def add_task():
+    pass
+    
+def display_tasks_list():
+    pass
+
+def update_task():
+    pass
+
+def mark_task():
+    pass
+
+def delete_task():
+    pass
     
 def main():
-    """Main function."""
-    check_file(file_path)
-    print("Welcome to \"Trackio\"! Type \"help\" to see available commands. Type \"exit\" to quit.\n")
+    #make a parser
+    parser = argparse.ArgumentParser(
+        prog="Trackio - Your task tracking helper",
+        description="App used to track and manage your tasks",
+        epilog=""
+    )
+    parser.add_argument('-a','--add',help='Add a task', required=False)
+    parser.add_argument('-ls','--list',help='-l [todo|all|done|in-progress] for sowing tasks', required=False)
+    parser.add_argument('-u', '--update', help='Type new description or something you want', required=False)
+    parser.add_argument('-m', '--mark', help='Type "in-progress" or "done"', required=False)
+    parser.add_argument('-d', '--delete', help='Delete', required=False)
     
-    while True:
-        try:
-            user_input = input("trackio >>> ")
-        except (KeyboardInterrupt, EOFError):
-            print("\nClosing Trackio.")
-            break
-        
-        if user_input.strip().lower() in {"exit", "quit"}:
-            print("Goodbye!")
-            break
-        if user_input.strip().lower() == "help":
-            print("""Available Commands:
-                - add <description>
-                - delete <id>
-                - update <id> "new task description"
-                - mark-in-progress
-                - mark-done
-                - list [all|done|in-progress|todo]
-                - exit/quit
-                """)
-            continue
+    args = parser.parse_args()
+    check_file(file_path)
+    if args.add:
+        add_task()
+
+    elif args.list:
+        display_tasks_list(str(args.list))
+
+    elif args.pick:
+        if args.update:
+            update_task()
+        elif args.mark:
+            mark_task()
+        elif args.delete:
+            delete_task()
+        else:
+            parser.error('For pick, you must specify either --update or --mark')
+
+    else:
+        exit()
         
             
 if __name__ == '__main__':
