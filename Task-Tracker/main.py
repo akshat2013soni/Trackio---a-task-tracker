804236db52760133008fbce68b330681a0d4a0cd
Akshat Soni <19uec087@lnmiit.ac.in>
2025-09-29 16:05:20 +0530

    Redefining commands and checking if files, create other wise

--- Task-Tracker/main.py
+++ Task-Tracker/main.py
@@ -1,54 +1,42 @@
-class Task:
-    def __init__(self):
+import json
+
+file_path = "tasks_list.json"
+   
+def check_file(file_path):
+    """To check file exits or not and creates if doe not"""
+    try:
+        with open(file_path, 'x'):
+            pass
+    except FileExistsError:
         pass
     
-def initial_input():
-    return input("What do you need to do today?\n1. Add a task\n2. Update a task\n3. Delete a task\n4. Change task status\n5. List all tasks\n6. List all tasks that are done\n7. List all tasks that are not done\n8. List all tasks that are in progress\n9. Exit\n")
-
-def add_task():
-    pass
-
-def update_task():
-    pass
-
-def delete_task():
-    pass
-
-def change_task_status():
-    pass
-
-def list_tasks():
-    pass
-    
-    
 def main():
-    print("Welcome to Trackio! - Your task tracker")
-    task_work = initial_input()
-    while not task_work.isnumeric():
-        print("Please enter the number corresponding to the task")
-        task_work = initial_input()
-    task_number = int(task_work)
-    if task_number == 1:
-        add_task()
-    elif task_number == 2:
-        update_task()
-    elif task_number == 3:
-        delete_task()
-    elif task_number == 4:
-        change_task_status()
-    elif task_number == 5:
-        list_tasks()
-    elif task_number == 6:
-        list_tasks()
-    elif task_number == 7:
-        list_tasks()
-    elif task_number == 8:
-        list_tasks()
-    elif task_number == 9:
-        print("Closing")
-    else:
-        print("Please choose from the list of options!\nMore options to be added soon!")
-                
+    """Main function."""
+    check_file(file_path)
+    print("Welcome to \"Trackio\"! Type \"help\" to see available commands. Type \"exit\" to quit.\n")
+    
+    while True:
+        try:
+            user_input = input("trackio >>> ")
+        except (KeyboardInterrupt, EOFError):
+            print("\nClosing Trackio.")
+            break
+        
+        if user_input.strip().lower() in {"exit", "quit"}:
+            print("Goodbye!")
+            break
+        if user_input.strip().lower() == "help":
+            print("""Available Commands:
+                - add <description>
+                - delete <id>
+                - update <id> "new task description"
+                - mark-in-progress
+                - mark-done
+                - list [all|done|in-progress|todo]
+                - exit/quit
+                """)
+            continue
+        
             
 if __name__ == '__main__':
     main()
